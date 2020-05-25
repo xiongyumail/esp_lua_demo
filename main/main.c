02bee06e8f98f847d32927eef446ec2c37b8b94f
xiongyu <xiongyu@espressif.com>
2020-05-25 11:27:59 +0800

    cli test ok

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -10,8 +11,69 @@
 #include "esp_log.h"
 #include "esp_lua.h"
 #include "esp_lua_lib.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "main";
+
+static int gpio_lua_init(lua_State *L) 
+{
+    int ret = -1;
+    gpio_num_t io = (gpio_num_t)luaL_checknumber(L,1);
+    char *mode = luaL_checklstring(L, 2, NULL);
+
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+
+    if (strcmp(mode, "OUTPUT") == 0) {
+        io_conf.mode = GPIO_MODE_OUTPUT;
+    } else if (strcmp(mode, "INPUT") == 0) {
+        io_conf.mode = GPIO_MODE_INPUT;
+    } else {
+        io_conf.mode = GPIO_MODE_OUTPUT;
+    }
+    
+    io_conf.pin_bit_mask = 1ULL << io;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    ret = gpio_config(&io_conf);
+
+    if (ret == 0) {
+        lua_pushboolean(L, true);
+    } else {
+        lua_pushboolean(L, false);
+    }
+    return 1;
+}
+
+static int gpio_lua_write(lua_State *L) 
+{
+    int ret = -1;
+    gpio_num_t io = (gpio_num_t)luaL_checknumber(L,1);
+    uint32_t level = luaL_checknumber(L,2);
+
+    ret = gpio_set_level(io, level);
+    
+    if (ret == 0) {
+        lua_pushboolean(L, true);
+    } else {
+        lua_pushboolean(L, false);
+    }
+    return 1;
+}
+
+static const luaL_Reg gpio_lib[] = {
+    {"init", gpio_lua_init},
+    {"write", gpio_lua_write},
+    {NULL, NULL}
+};
+
+LUAMOD_API int esp_lib_gpio(lua_State *L) 
+{
+    luaL_newlib(L, gpio_lib);
+    lua_pushstring(L, "0.1.0");
+    lua_setfield(L, -2, "_version");
+    return 1;
+}
 
 static const luaL_Reg mylibs[] = {
     {"sys", esp_lib_sys},
@@ -19,6 +81,7 @@ static const luaL_Reg mylibs[] = {
     {"web", esp_lib_web},
     {"mqtt", esp_lib_mqtt},
     {"httpd", esp_lib_httpd},
+    {"gpio", esp_lib_gpio},
     {NULL, NULL}
 };
 
--- main/main.lua
+++ main/main.lua
@@ -1,70 +1,25 @@
-local json = require('json')
-local dump = require('dump')
-local wifi = require('wifi')
+json = require('json')
+dump = require('dump')
+wifi = require('wifi')
 
-print(dump.table(sys.info()))
-if (not wifi.start_sta('ESP', '123456789')) then
-    print('Connect to AP and log in to http://192.168.1.1 and configure router information')
-    wifi.start_ap('ESP_LUA', '')
-end
-print(dump.table(net.info()))
-assert(sys.sntp('ntp1.aliyun.com'))
-print(os.date("%Y-%m-%d %H:%M:%S"))
-
-local key = '&key=lsawo7f7smtdljg9'
-
-local base_url  = 'http://api.seniverse.com/v3/weather/now.json?language=en&unit=c'..key
-
-print(base_url)
-
-local mqtt_connected = false
-mqtt.start('mqtt://mqtt.emake.run')
-local last_clock = 0
-local info = {['loop'] = 0}
 while (1) do
-    local handle = mqtt.run()
-    if (handle) then
-        if (handle.event == 'MQTT_EVENT_DATA') then
-            local t = json.decode(handle.data)
-            print(dump.table(t))
-            if (handle.topic == 'sys') then
-                if (handle.data == 'stop') then -- mosquitto_pub -h mqtt.emake.run -t sys -m "\"stop\""
-                    mqtt.stop()
-                    break
-                end
+    s = ''
+    while (1) do
+        c = io.read(1)
+        if (c) then
+            s = s..c
+            if (c == '\n') then
+                break
             end
-        elseif (handle.event == 'MQTT_EVENT_CONNECTED') then
-            mqtt_connected = true
-            mqtt.sub('nc_temp', 0)
-            mqtt.sub('sh_temp', 0)
-            mqtt.sub('sys', 0)
-        elseif (handle.event == 'MQTT_EVENT_DISCONNECTED') then
-            mqtt_connected = false
+        else
+            sys.yield()
         end
     end
-    if (mqtt_connected and os.difftime (os.time(), last_clock) >= 30) then
-        local nc_temp = web.rest('GET', base_url..'&location=nanchang')
-        if (nc_temp) then
-            mqtt.pub('nc_temp', nc_temp, 0)
-        end
-        local sh_temp = web.rest('GET', base_url..'&location=shanghai')
-        if (sh_temp) then
-            mqtt.pub('sh_temp', sh_temp, 0)
-        end
-        info.clock = os.clock()
-        info.date = os.date("%Y-%m-%d %H:%M:%S")
-        info.info = sys.info()
-        mqtt.pub('test', json.encode(info), 1)
-        last_clock = os.time()
-    end
-    
-    info.clock = os.clock()
-    info.date = os.date("%Y-%m-%d %H:%M:%S")
-    info.info = sys.info()
-    info.loop = info.loop + 1
-    print(json.encode(info))
 
-    if (not handle) then
-        sys.yield()
+    f = load('return '..s)
+    if (f) then
+        print(f())
+    else
+        print('false')
     end
 end
